70f1ed81f940c3801adcab16ba650eb3ee44e40b
swanpyaesone26 <joyboyop69@gmail.com>
2026-02-19 20:34:11 +0700

    feat (Main App) : Init main app

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
+    'rest_framework',
+    'django_filters',
+    # Local apps
+    'app.src',
 ]
 
 MIDDLEWARE = [
