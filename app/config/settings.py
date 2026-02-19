487bbbe4d8dcbd89229c3e274000db280ce72489
swanpyaesone26 <joyboyop69@gmail.com>
2026-02-19 20:38:25 +0700

    style : Restructure the folder

--- app/config/settings.py
+++ app/config/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'app.config.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'app.config.wsgi.application'
 
 
 # Database
