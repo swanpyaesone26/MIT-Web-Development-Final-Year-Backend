2f61c289a63a3e09491328a2167d5bddd89b4393
swanpyaesone26 <joyboyop69@gmail.com>
2026-02-24 11:09:52 +0700

    feat : Finish Teacher DB table'

--- app/src/models.py
+++ app/src/models.py
@@ -7,6 +7,11 @@ class Teacher(models.Model):
     teacher_id = models.AutoField(primary_key=True)
     teacher_name = models.CharField(max_length=100)
     subject_id = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    year_id =models.ForeignKey(Year,on_delete=models.CASCADE)
+    password = models.CharField(max_length=100)
+    room_id = models.ForeignKey(Room,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 
