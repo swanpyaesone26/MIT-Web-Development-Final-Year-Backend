74ecfea129bab473862300e090bd804b6530483d
swanpyaesone26 <joyboyop69@gmail.com>
2026-02-24 11:06:31 +0700

    feat : Add db tables names

--- app/src/models.py
+++ app/src/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+#teacher db
+
+class Teacher(models.Model):
+    teacher_id = models.AutoField(primary_key=True)
+    teacher_name = models.CharField(max_length=100)
+    subject_id = models.ForeignKey(Subject,on_delete=models.CASCADE)
+
+
+
+
+
+# student db
+class Student(models.Model):
+
+
+
+
+
+
+#assignment db
+class Assignment(models.Model):
+
+
+
+
+#year db
+class Year(models.Model):
+
+
+#room db
+class Room(models.Model):
+
+
+
+#subject db
+class Subject(models.Model):
+
+
+
+
