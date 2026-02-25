5cd61a50ade34b34fab047510f66e20671250cdf
swanpyaesone26 <joyboyop69@gmail.com>
2026-02-25 12:52:11 +0700

    Fix : Fix serializer class type

--- app/src/serializers.py
+++ app/src/serializers.py
@@ -2,37 +2,37 @@ from rest_framework import serializers
 from app.src.models import Student, Teacher, Assignment, Year, Room, Subject
 
 # teacher serializer
-class TeacherSerializer(serializers.Serializer):
+class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = '__all__'
 
 # student serializer    
-class StudentSerializer(serializers.Serializer):
+class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
 
 # assignment serializer
-class AssignmentSerializer(serializers.Serializer):
+class AssignmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assignment
         fields = '__all__'
 
 # year serializer
-class YearSerializer(serializers.Serializer):
+class YearSerializer(serializers.ModelSerializer):
     class Meta:
         model = Year
         fields = '__all__'
 
 # room serializer
-class RoomSerializer(serializers.Serializer):
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = '__all__'
 
 # subject serializer
-class SubjectSerializer(serializers.Serializer):
+class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = '__all__'
