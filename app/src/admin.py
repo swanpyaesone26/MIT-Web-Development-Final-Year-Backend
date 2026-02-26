702586bfba1d555e429eecd78c8b30f99c3caf35
swanpyaesone26 <joyboyop69@gmail.com>
2026-02-26 09:54:40 +0700

    feat (Admin) : Add based features for registered models

--- app/src/admin.py
+++ app/src/admin.py
@@ -22,3 +22,36 @@ class UserAdmin(BaseUserAdmin, ModelAdmin):
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
     pass
+
+# need to register, maybe we do next this and other user models
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('teacher_id', 'teacher_name', 'subject', 'year', 'created_at', 'updated_at')
+    search_fields = ('teacher_name','subject','year')
+    ordering = ('year',)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'student_name', 'year', 'created_at', 'updated_at')
+    search_fields = ('student_name','year')
+    ordering = ('year',)
+
+@admin.register(Year)
+class YearAdmin(admin.ModelAdmin):
+    list_display = ('year_id', 'year_name', 'created_at', 'updated_at')
+    search_fields = ('year_name',)
+    ordering = ('year_name',)
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('subject_id', 'subject_name', 'created_at', 'updated_at')
+    search_fields = ('subject_name',)
+    ordering = ('subject_name',)
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('room_id', 'room_name', 'created_at', 'updated_at')
+    search_fields = ('room_name',)
+    ordering = ('year',)
+
+
