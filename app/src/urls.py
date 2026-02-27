5e167268e4cd7133c6c0a1b7b5a0cab2928efd4c
swanpyaesone26 <joyboyop69@gmail.com>
2026-02-28 00:15:51 +0700

    feat (Auth) : Add api endpoints

--- app/src/urls.py
+++ app/src/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    
+    # JWT auth endpoints
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
