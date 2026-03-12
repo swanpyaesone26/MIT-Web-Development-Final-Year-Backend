0e085f2603775cf16939ed4e0eb70582ab72cdef
swanpyaesone26 <joyboyop69@gmail.com>
2026-03-12 14:22:28 +0700

    build : Fix s3 config

--- app/config/settings.py
+++ app/config/settings.py
@@ -246,7 +246,8 @@ if USE_S3:
     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None  # No public ACL; access controlled per file via signed URLs
+    AWS_QUERYSTRING_AUTH = True  # Generate signed URLs for private file access
     
     # S3 media files
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
